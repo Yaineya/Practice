7fa84c5528ba18bcf961ffff10fada65bf0879f6
Yaineya <evgeniydorofeev@MacBook-Air-ll.local>
2020-09-17 16:29:12 +0300

    es lint tested

--- kilometersToMeters.js
+++ kilometersToMeters.js
@@ -1,20 +1,22 @@
 function kmh_ms(kmH){
     let result = (kmH * 1000 / 3600).toFixed(2);
-    return parseFloat(result);
+    result = parseFloat(result);
+    return result;
 } 
 
-function ms_kmh(ms){
-    let result =  (ms * 3.6).toFixed(2);
+function ms_kmh(ms) 
+{
+    const result = (ms * 3.6) .toFixed(2);
     return parseFloat(result);
 }
 
 module.exports = {
     // isNumber: isNumber,
-    kmh_ms: kmh_ms,
-    ms_kmh: ms_kmh
-}
+    kmh_ms,
+    ms_kmh
+};
 
-// kmh_ms(200);
+//kmh_ms(200);
 // kmh_ms(20);
 // kmh_ms(120);
 
